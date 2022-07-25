39782e6ee571530bda9b4e444fa149fe148efd05
Sebonze <sebonze3317@gmail.com>
2022-07-25 14:52:49 +0200

    premier graph

---
+++ plot_test.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.figure()
+plt.plot([1,2,3,4,5], [10,20,30,40,50])
+
+print("on va ajouter des légendes sur le graphe")
+plt.xlabel("unités")
+plt.ylabel("dizaines")
+
+
+plt.show()
